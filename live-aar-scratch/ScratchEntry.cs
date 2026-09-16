b4f68d09264fd9b5576eaaab0878b5f023c3e4c6
Toxic_Cxnt <kyedominic40@gmail.com>
2026-09-17 00:14:17 +0100

    Report correct scratch v1.0.3 version

--- live-aar-scratch/ScratchEntry.cs
+++ live-aar-scratch/ScratchEntry.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(GHPCNativeLiveAAR.Mod), "GHPC Native Live AAR", "1.0.0-scratch", "Toxic_Cxnt")]
+[assembly: MelonInfo(typeof(GHPCNativeLiveAAR.Mod), "GHPC Native Live AAR", "1.0.3-scratch", "Toxic_Cxnt")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace GHPCNativeLiveAAR
